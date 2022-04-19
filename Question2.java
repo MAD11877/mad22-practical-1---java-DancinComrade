481a1782b8b59dd0e9644889afe36e781762b21a
DancinComrade <63692048+DancinComrade@users.noreply.github.com>
2022-04-19 12:35:07 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -11,9 +11,7 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
-    System.out.println("Enter height (in metres): ");
     double height = in.nextDouble();
-    System.out.println("Enter weight (in kg): ");
     double weight = in.nextDouble();
     double bmi = weight / (height * height);
     System.out.println(bmi);
