6a3c65a5b00847f0972f8286d6faec0da4f4e233
DancinComrade <63692048+DancinComrade@users.noreply.github.com>
2022-04-19 20:02:36 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -26,7 +26,40 @@ public class Question5
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
      
-    Scanner in = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
+    int numberInput = input.nextInt();
+
+    int[] numArray = new int[numberInput];
     
+    for (int x = 0; x < numArray.length; x++) {
+      int numArrInp = input.nextInt();
+      numArray[x] = numArrInp;
+    }
+
+    int currentNum = 0;
+    int mode = 0;
+    int count = 0;
+    int modeCount = 0;
+
+    // Loop through array
+    for (int i = 0; i < numArray.length; i++) {
+      
+      // Select one number and compare with the rest in       the array
+      currentNum = numArray[i];
+
+      for (int y = 0; y < numArray.length; y++) {
+        if (currentNum == numArray[y]) {
+
+          count++;
+        }  
+      }
+      
+      if (count > modeCount) {
+        mode = currentNum;
+        modeCount = count;
+      }
+      count = 0;
+    }
+    System.out.println(mode);
   }
 }
